706545a36481e38b8943d6e1cd6702b18e506ef4
mmallemmann@gmail.com <ipsum.rex@gmail.com>
2018-04-02 14:29:54 +0100

    wrote analyis script

--- helpers/circmean.m
+++ helpers/circmean.m
@@ -1,5 +1,5 @@
-function val = circmean(X,dim,nanflag)
-% val = circmean(X[,dim,nanflag])
+function [val,mag] = circmean(X,dim,nanflag)
+% [val,mag] = circmean(X[,dim,nanflag])
 %
 % Calculate the mean of points that lie on a circle
 % Takes mean along longest dimension by default
@@ -10,6 +10,8 @@ if nargin<3, nanflag = false; end
 % place along unit circle, take mean, get phase
 if nanflag 
     val = angle(nanmean(exp(1i*X),dim));
+    mag = abs(nanmean(exp(1i*X),dim));
 else
     val = angle(mean(exp(1i*X),dim));
+    mag = abs(mean(exp(1i*X),dim));
 end
